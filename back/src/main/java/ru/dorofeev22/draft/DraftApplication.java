30d44b15cc72214cdb96e7fac64f27870663a4d1
Юрий Дорофеев <dyy@dimsoft.ru>
2021-03-10 19:14:32 +0700

    successful build with front using maven plugin

---
+++ back/src/main/java/ru/dorofeev22/draft/DraftApplication.java
@@ -0,0 +1,13 @@
+package ru.dorofeev22.draft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DraftApplication.class, args);
+	}
+
+}
