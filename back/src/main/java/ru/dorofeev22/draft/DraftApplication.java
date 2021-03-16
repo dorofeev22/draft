8c1c6ece39d7e727fe4742b072032423c2a06792
Александр Ручкин <raa@dimsoft.ru>
2021-03-16 10:51:53 +0700

    #comment add swagger

--- back/src/main/java/ru/dorofeev22/draft/DraftApplication.java
+++ back/src/main/java/ru/dorofeev22/draft/DraftApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import ru.dorofeev22.draft.core.config.ModelMapperConfig;
 import ru.dorofeev22.draft.core.config.ObjectMapperConfig;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
+@EnableSwagger2
 @SpringBootApplication
-@Import({ModelMapperConfig.class, ObjectMapperConfig.class})
+@Import({ModelMapperConfig.class, ObjectMapperConfig.class, WebMvcConfig.class, SwaggerConfig.class})
 public class DraftApplication {
 
 	public static void main(String[] args) {
